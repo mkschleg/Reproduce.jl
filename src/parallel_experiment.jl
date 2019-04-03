469c12ef674ddb5fdbd70aff1d35bacbe36e0aa0
Matthew Schlegel <mkschleg@gmail.com>
2019-04-03 17:30:27 -0600

    Updated interface for parallel experiments

--- src/parallel_experiment.jl
+++ src/parallel_experiment.jl
@@ -6,10 +6,32 @@ using Logging
 const IN_SLURM = "SLURM_JOBID" in keys(ENV)
 IN_SLURM && using ClusterManagers
 
-function parallel_experiment(experiment_file, args_iter; exp_module_name=:Main, exp_func_name=:main_experiment, num_workers=5, expand_args=false)
+
+function job(experiment_file, args_iter; exp_module_name=:Main, exp_func_name=:main_experiment, num_workers=5, expand_args=false)
+
+    if "SLURM_ARRAY_TASK_ID" in keys(ENV)
+        @info "This is an array Job! Time to "
+        task_id = parse(Int64, ENV["SLURM_ARRAY_TASK_ID"])
+        @time task_job(experiment_file, args_iter, task_id;
+                       exp_module_name=exp_module_name,
+                       exp_func_name=exp_func_name,
+                       expand_args=expand_args)
+    else
+        @time parallel_job(experiment_file, args_iter;
+                           exp_module_name=exp_module_name,
+                           exp_func_name=exp_func_name,
+                           num_workers=num_workers,
+                           expand_args=expand_args)
+    end
+
+end
+
+
+function parallel_job(experiment_file, args_iter; exp_module_name=:Main, exp_func_name=:main_experiment, num_workers=5, expand_args=false)
 
     pids = Array{Int64, 1}
     if IN_SLURM
+        # assume started fresh julia instance...
         pids = addprocs(SlurmManager(parse(Int, ENV["SLURM_NTASKS"])))
         print("\n")
     else
@@ -73,3 +95,20 @@ function parallel_experiment(experiment_file, args_iter; exp_module_name=:Main, 
     end
 
 end
+
+function task_job(experiment_file, args_iter, task_id; exp_module_name=:Main, exp_func_name=:main_experiment, expand_args=false)
+
+    include(exp_file)
+    @info "$(exp_file) included for Job $(task_id)"
+    exp_func = getfield(getfield(Main, Symbol($mod_str)), Symbol($func_str))
+    @info "Running $(task_it)"
+    args = collect(args_iter)[task_id]
+    if expand_args
+        exp_func(args...)
+    else
+        exp_func(args)
+    end
+
+end
+
+
