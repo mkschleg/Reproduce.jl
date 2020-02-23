d0b709432d44bc7a1b08e681c29b95e63d204862
Matthew Schlegel <mkschleg@cedar5.cedar.computecanada.ca>
2020-02-23 10:58:07 -0800

    added slurm task example

--- examples/parallel_slurm_task.jl
+++ examples/parallel_slurm_task.jl
@@ -1,7 +1,5 @@
 #!/cvmfs/soft.computecanada.ca/easybuild/software/2017/avx2/Compiler/gcc7.3/julia/1.3.0/bin/julia
 #SBATCH --array=1-200
-#SBATCH --mail-user=mkschleg@ualberta.ca
-#SBATCH --mail-type=ALL
 #SBATCH -o reproduce_test.%A_%a.out # Standard output
 #SBATCH -e reproduce_test.%A_%a.err # Standard error
 #SBATCH --mem-per-cpu=512M # Memory request of 2 GB
@@ -14,18 +12,7 @@ Pkg.activate(".")
 using Reproduce
 const config_file = "./examples/arg_iter_config.toml"
 
-function test_experiment()
-
-    as = ArgParseSettings()
-    @add_arg_table as begin
-        "task_id"
-        arg_type=Int
-    end
-    parsed = parse_args(as)
     
-    experiment = Experiment(config_file)
-    create_experiment_dir(experiment; tldr="hello tldr")
-    ret = job(experiment, parsed["task_id"])
-end
-
-test_experiment()
+experiment = Experiment(config_file)
+create_experiment_dir(experiment; tldr="hello tldr")
+ret = job(experiment)
