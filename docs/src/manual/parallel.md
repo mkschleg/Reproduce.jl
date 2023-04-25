1780c522518fd5ee90015ca5c292f9c554178b2c
Matthew Schlegel <mkschleg@gmail.com>
2023-04-25 10:50:12 -0600

    Updating docs, working on comp_env documentation.

--- docs/src/manual/parallel.md
+++ docs/src/manual/parallel.md
@@ -1,3 +1,30 @@
 # Running a parallel job
 
 
+I include an example file for running parallel jobs locally and in a SLURM environment. There are several paths this experiment can be used, all with automatic checkpointing so jobs can be resumed.
+
+There are four main functions that you need to be aware of for running parallel jobs from a config file:
+[`parse_experiment_from_config`](@ref), [`pre_experiment`](@ref), [`job`](@ref), [`post_experiment`](@ref).
+
+## Parsing the experiment
+
+Using the config specification and argument specifications found through the parse docs, you use [`parse_experiment_from_config`](@ref) to parse your config and point Reproduce to where the base directory will be located. On HPC clusters this will usually be a scratch directory. This will be the home directory for the directory listed in the config file, not the root directory of all the job files.
+
+There are some kwargs which are optional. One is `comp_env` which should only be set if you have a particular need and know what you are doing. Otherwise `comp_env` will be automatically set by [`get_comp_env`](@ref).
+
+
+## pre_experiment
+
+This function does all the setup, and needs to be done on the bash script node. There could be overlap if not.
+
+## job
+
+This runs the job using the comp_env decided on when parsing the experiment (see above).
+
+## post_experiment
+
+This is meant to do cleanup, but doesn't do anything right now.
+
+
+
+
--- src/comp_envs.jl
+++ src/comp_envs.jl
@@ -26,6 +26,12 @@ get_job_name(comp_env) = "job"
 get_job_name(comp_env::SlurmParallel) = comp_env.job_name
 get_job_name(comp_env::SlurmTaskArray) = comp_env.job_name * "_$(comp_env.array_idx)"
 
+
+"""
+    get_comp_env
+
+This derives the computational environment from the ENV variables. If in a slurm job the [`get_slurm_comp_env`](@ref) is used, if not [`get_local_comp_env`](@ref) is used.
+"""
 function get_comp_env(; kwargs...) 
     if IN_SLURM()
         get_slurm_comp_env(; kwargs...)
@@ -34,6 +40,11 @@ function get_comp_env(; kwargs...)
     end
 end
 
+"""
+    get_local_comp_env
+
+This checks to see if `RP_TASK_ID` is set in the environment. If so, a Task Job with `ID=parse(Int, "RP_TASK_ID")` will be returned. Otherwise, LocalParallel will be used. The kwargs (`num_workers` and `threads_per_worker`) give the job defaults for the number of parallel jobs and the number of threads per task. You can also use the `ENV` variables "RP_NTASKS" and "RP_CPUS_PER_TASK" to override these. The `ENV` variables will take precedence.
+"""
 function get_local_comp_env(; num_workers=Sys.CPU_THREADS - 1, threads_per_worker=1, kwargs...)
 
     if "RP_TASK_ID" ∈ keys(ENV)
@@ -46,6 +57,36 @@ function get_local_comp_env(; num_workers=Sys.CPU_THREADS - 1, threads_per_worke
     
 end
 
+
+"""
+    get_slurm_comp_env
+
+This is significantly more complex than a local environment to enable using slurm task arrays efficiently.
+
+In a job scheduled as:
+```sh
+sbatch -J test_rep_argiter --ntasks 4 --cpus-per-task 1 --mem-per-cpu=2000M --time=0:10:00 toml_parallel.jl configs/arg_iter_config.toml --path /home/mkschleg/scratch/reproduce
+```
+Then the comp_env will return a `SlurmParallel` env which uses `srun` to create julia instances.
+
+For
+```sh
+sbatch -J test_rep_argiter -N 1 --ntasks 1 --cpus-per-task 4 --mem-per-cpu=2000M --time=0:10:00 toml_parallel.jl configs/arg_iter_config.toml --path /home/mkschleg/scratch/reproduce
+```
+The comp env will be a local parallel job using just default julia parallel utilities. It is necessary to make sure all resources are on single node if using this.
+
+For
+```sh
+sbatch -J test_rep_argiter --array=1-4 --ntasks 4 --cpus-per-task 1 --mem-per-cpu=2000M --time=0:05:00 toml_parallel.jl configs/arg_iter_config.toml --path /home/mkschleg/scratch/reproduce
+```
+The comp env will be a SlurmTaskArray which will choose either LocalParallel, or SlurmParallel following the above protocol.
+
+*Notes:*
+1. If no `SLURM_CPUS_PER_TASK` is set then we assume a single cpu per task.
+2. If you are re-running only parts of an array task you need to use "RP_CUSTOM_ARRAY_TASK_COUNT" to let Reproduce know what the original task array looked like to schedule the jobs correctly.
+
+
+"""
 function get_slurm_comp_env(; kwargs...)
     if "SLURM_ARRAY_TASK_ID" ∈ keys(ENV)
 
@@ -59,20 +100,26 @@ function get_slurm_comp_env(; kwargs...)
         cpus_per_task = parse(Int, get(ENV, "SLURM_CPUS_PER_TASK", "1"))
 
         prl = if ntasks == 1
-            # check if RP_ONE_PARAM is set
-            if "RP_TASK_ID" ∈ keys(ENV) || cpus_per_task > 1
+            # check if RP_TASK_ID is set or there is only a single cpu in the task
+            if "RP_TASK_ID" ∈ keys(ENV) || cpus_per_task == 1
                 TaskJob(parse(Int, "RP_TASK_ID"))
             else
                 # otherwise do local parallel (i.e. only on a signle node!).
-                LocalParallel(ntasks, cpus_per_task)
+                LocalParallel(cpus_per_task, 1)
             end
         else
             SlurmParallel(ntasks, cpus_per_task, ENV["SLURM_JOB_NAME"])
         end
         SlurmTaskArray(array_id, prl, array_size, ENV["SLURM_JOB_NAME"])
     else
-        SlurmParallel(parse(Int, ENV["SLURM_NTASKS"]),
-                      parse(Int, get(ENV, "SLURM_CPUS_PER_TASK", "1")),
-                      ENV["SLURM_JOB_NAME"])
+        ntasks = parse(Int, ENV["SLURM_NTASKS"])
+        cpus_per_task = parse(Int, get(ENV, "SLURM_CPUS_PER_TASK", "1"))
+        if ntasks == 1
+            LocalParallel(cpus_per_task, 1)
+        else
+            SlurmParallel(ntasks,
+                          cpus_per_task,
+                          ENV["SLURM_JOB_NAME"])
+        end
     end
 end
